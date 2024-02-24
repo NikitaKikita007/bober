e03ce4285316fd44f552cf40cc825c0bf6acd54a
Quarkily <157905032+Quarkily@users.noreply.github.com>
2024-02-24 18:56:18 +0200

    ers

---
+++ jsstart/main1.js
@@ -0,0 +1,46 @@
+// console.log("Start")
+
+// let a = 1
+// const b = 2
+// a = 2
+// let mama = "mother"
+// const papa = "alex"
+// let nomer = "2"
+// console.log(a)
+// console.log("a")
+
+// console.log(a+b+a)
+// console.log(10+23+a)
+// console.log(mama+2+papa)
+
+// console.log(nomer+2-2)
+
+// console.log("2"+2)
+// console.log(2+"2"-2)
+// console.log(mama-2)
+// // Nan - Not A Number
+// // undefined
+// // True/False
+// console.log("2"==2)
+// console.log(papa.length)
+// console.log(papa.indexOf("l"))
+
+// let A = 2
+
+// if (A == 2)(
+//     let result = A*A
+// )
+
+
+
+
+
+
+let userAnswer = +prompt("chislo vvedi")
+console.log(typeof(userAnswer))
+
+if (isNaN(userAnswer)){
+    confirm("help")
+    alert("plesae")
+
+}
