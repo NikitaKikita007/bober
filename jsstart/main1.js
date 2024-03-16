09e915c51df6dd30b064827c516cf014df97d8e9
Matvey091 <157905032+Quarkily@users.noreply.github.com>
2024-03-16 18:51:43 +0200

    poasttgcd

--- jsstart/main1.js
+++ jsstart/main1.js
@@ -47,84 +47,112 @@
 
 
 
-const button = document.getElementById('multiply')
-const firstNum = document.getElementById('firstNum')
-const secondNum = document.getElementById('secondNum')
+// const button = document.getElementById('multiply')
+// const firstNum = document.getElementById('firstNum')
+// const secondNum = document.getElementById('secondNum')
 
-function multiply(){
-    let result = firstNum.value * secondNum.value
-    console.log(result)
+// function multiply(){
+//     let result = firstNum.value * secondNum.value
+//     console.log(result)
+// }
+
+// button.addEventListener('click', multiply)
+
+
+
+
+
+
+// const button1 = document.getElementById('divide')
+// const firstNum1 = document.getElementById('firstNum1')
+// const secondNum1 = document.getElementById('secondNum1')
+
+// function divide(){
+//     let result = firstNum1.value / secondNum1.value
+
+//     if (secondNum1.value == 0){
+//         let result2 = "На ноль не делиться гуний"
+//         console.log(result2)
+//     }
+//     else{
+//         console.log(result)
+//     }
+// }
+
+// button1.addEventListener('click', divide)
+
+
+
+
+
+
+// const button2 = document.getElementById('add')
+// const firstNum2 = document.getElementById('firstNum2')
+// const secondNum2 = document.getElementById('secondNum2')
+
+// function add(){
+//     let result = parseInt(firstNum2.value) + parseInt(secondNum2.value)
+//     console.log(result)
+// }
+
+// button2.addEventListener('click', add)
+
+
+
+
+
+
+
+
+
+// const button3 = document.getElementById('minus')
+// const firstNum3 = document.getElementById('firstNum3')
+// const secondNum3 = document.getElementById('secondNum3')
+
+// function minus(){
+//     let result = firstNum3.value - secondNum3.value
+//     console.log(result)
+// }
+
+// button3.addEventListener('click', minus)
+
+
+
+
+// const button4 = document.getElementById('sqrt')
+// const firstNum4 = document.getElementById('firstNum4')
+
+// function sqrot(){
+//     let result = Math.sqrt(firstNum4.value)
+//     console.log(result)
+// }
+
+// button4.addEventListener('click', sqrot)
+
+
+
+let post = "1234qwerty@gmail.com"
+
+// console.log(post.length)
+
+if (isNaN(Number(post.charAt(0)))) {
+    console.log("first argument is not a number")
+}
+console.log(post.indexOf("@gmail.com"))
+if (post.indexOf("@gmail.com")>0){
+    console.log("thats gmail")
+}
+// console.log("A"==("a").toUpperCase())
+// let a = "baNaNas"
+
+// console.log(a.toLowerCase())
+// console.log(a.toUpperCase())
+console.log(post.includes(" "))
+console.log(post.includes("@"))
+console.log(post.includes("."))
+console.log(post.includes("com"))
+
+if (post.indexOf("l") > 0){
+    console.log("true")
 }
 
-button.addEventListener('click', multiply)
-
-
-
-
-
-
-const button1 = document.getElementById('divide')
-const firstNum1 = document.getElementById('firstNum1')
-const secondNum1 = document.getElementById('secondNum1')
-
-function divide(){
-    let result = firstNum1.value / secondNum1.value
-
-    if (secondNum1.value == 0){
-        let result2 = "На ноль не делиться гуний"
-        console.log(result2)
-    }
-    else{
-        console.log(result)
-    }
-}
-
-button1.addEventListener('click', divide)
-
-
-
-
-
-
-const button2 = document.getElementById('add')
-const firstNum2 = document.getElementById('firstNum2')
-const secondNum2 = document.getElementById('secondNum2')
-
-function add(){
-    let result = parseInt(firstNum2.value) + parseInt(secondNum2.value)
-    console.log(result)
-}
-
-button2.addEventListener('click', add)
-
-
-
-
-
-
-
-
-
-const button3 = document.getElementById('minus')
-const firstNum3 = document.getElementById('firstNum3')
-const secondNum3 = document.getElementById('secondNum3')
-
-function minus(){
-    let result = firstNum3.value - secondNum3.value
-    console.log(result)
-}
-
-button3.addEventListener('click', minus)
-
-
-
-
-const button4 = document.getElementById('sqrt')
-const firstNum4 = document.getElementById('firstNum4')
-
-function sqrot(){
-    let result = Math.sqrt(firstNum4.value)
-    console.log(result)
-}
-
-button4.addEventListener('click', sqrot)
